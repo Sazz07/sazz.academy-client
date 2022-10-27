fd712535758268505d10431b9410e6c561fa0118
Sazz07 <sazzad.hossain882@gmail.com>
2022-10-27 16:01:09 +0600

    Firebase added with environment variable

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 
 const Checkout = () => {
     const courses = useLoaderData();
-    console.log(courses);
+    // console.log(courses);
     return (
         <section className="mt-6 text-gray-700">
             <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
@@ -11,7 +11,7 @@ const Checkout = () => {
                     <img src={courses.image_url} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500" />
                     <div className="p-12 space-y-5 lg:col-span-5">
                         <h3 className="text-2xl font-semibold sm:text-4xl">Thank You <span className='text-red-700'></span></h3>
-                        <p>For becoming an member to our premium of our site. You have selected- <br /><br /> <span className='font-medium text-red-700'>Course: <span className='text-lg text-violet-700'>{courses.title}.</span></span></p>
+                        <p>For becoming a premium member. You have selected- <br /><br /> <span className='font-medium text-red-700'>Course: <span className='text-lg text-violet-700'>{courses.title}.</span></span></p>
                     </div>
                 </div>
             </div>
