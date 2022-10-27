fb40104595f476c5898a35330f82996eec634b14
Sazz07 <sazzad.hossain882@gmail.com>
2022-10-27 13:36:59 +0600

    Added Error page component

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 export const routes = createBrowserRouter([
     {
         path: '/',
+        errorElement: <ErrorPage></ErrorPage>,
         element: <Main></Main>
     }
 ]);
